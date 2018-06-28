1d6c0241e9a5e1d423149abbd0ebd0228f9a1969
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-06-28 12:54:27 +0100

    first commit, adding index.html + js files

---
+++ js/fbq.js
@@ -0,0 +1,15 @@
+window.fbq = function fbq(value) {
+    "use strict";
+	
+	 if (typeof value !== "string") {
+        return fbq(String(value));
+    }
+	
+	
+
+  
+  
+ 
+	
+	
+}
---
+++ js/fbq.test.js
@@ -0,0 +1,8 @@
+
+
+QUnit.test( "hello Wenamity", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
+
+
+

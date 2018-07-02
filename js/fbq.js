59cd082fd5e7f3a405239febd5b380976fe5343b
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-07-02 17:28:12 +0100

    improving code + adding all required kata test

--- js/fbq.js
+++ js/fbq.js
@@ -1,4 +1,3 @@
-
 const ch= ' \u21D2 '; 
 window.fbq = function fbq(value) {
     "use strict";
@@ -9,45 +8,37 @@ window.fbq = function fbq(value) {
 	
 	var result='';
 	var isDivisable=false;
-	var isContain = false;
+	var isContains = false;
 
 	var Dictionary = new Map([
         ["3", "Foo"],
         ["5", "Bar"],
-        ["7", "Qix"]
+        ["7", "Qix"],
+		["0", "*"]
     ]);
 	
-	Dictionary.forEach(function (val, number) {
-        if (value % number === 0) {
-            result += val;
-			isDivisable=true;
-        }
+	Dictionary.forEach(function (val, number) { 
+		if (number!=='0'){
+			if (value % number === 0) {
+				result += val;
+				isDivisable=true;
+			}
+			if(value.indexOf(number) >-1)
+				 isContains=true;
+		}
     });
 	
-	
-	if ((value.indexOf('3') >-1)||(value.indexOf('5') >-1)|| (value.indexOf('7') >-1) ){
-        isContain=true;
-    }
-	 if(!isDivisable && !isContain)
+	 if(!isDivisable && !isContains)
 	  return value.replace(/0/g, '*');
 
 	// checking for more char occurences
 	 value.split("").forEach(function (num) { // forEach is the fastest array loop for chrome 60 
-        if (num === "3") {
-            result += "Foo";
-        }
-		 if (num === "5") {
-            result += "Bar";
-        }
-		 if (num === "7") {
-            result += "Qix";
-        }
-		 if (num === "0") {
-            result += "*";
+        if (Dictionary.get(num)) {
+            result += Dictionary.get(num);
         }
 		
     });
 	
-	  return result.length === 0 ? value : result;
+	  return result;
 	
 }
--- js/fbq.test.js
+++ js/fbq.test.js
@@ -3,34 +3,67 @@
 QUnit.test( "hello Wenamity", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
-
-
 QUnit.test( "fbq('1')", function( assert ) {
-  assert.ok( fbq(1) == "1", "1  "+ ch +"1" );
+  assert.ok( fbq(1) == "1", "1"+ ch +"1" );
 });
-
 QUnit.test( "fbq('2')", function( assert ) {
-  assert.ok( fbq('2') == "2", "2  "+ ch +"2" );
+  assert.ok( fbq('2') == "2", "2"+ ch +"2" );
 });
-
 QUnit.test( "fbq('3')", function( assert ) {
   assert.ok( fbq('3') == "FooFoo", "FooFoo (divisible by 3, contains 3)" );
 });
-
-QUnit.test( "fbq('32')", function( assert ) {
-  assert.ok( fbq('32') == "Foo", "Foo (contains 3)" );
+QUnit.test( "fbq('4')", function( assert ) {
+  assert.ok( fbq('4') == "4", "4 "+ ch +"4"  );
+});
+QUnit.test( "fbq('5')", function( assert ) {
+  assert.ok( fbq('5') == "BarBar", "BarBar (divisible by 5, contains 5)" );
+});
+QUnit.test( "fbq('6')", function( assert ) {
+  assert.ok( fbq('6') == "Foo", "Foo (divisible by 3)" );
+});
+QUnit.test( "fbq('7')", function( assert ) {
+  assert.ok( fbq('7') == "QixQix", "QixQix (divisible by 7, contains 7)" );
+});
+QUnit.test( "fbq('8')", function( assert ) {
+  assert.ok( fbq('8') == "8", "8 "+ ch +"8"  );
+});
+QUnit.test( "fbq('9')", function( assert ) {
+  assert.ok( fbq('9') == "Foo", "Foo" );
+});
+QUnit.test( "fbq('10')", function( assert ) {
+  assert.ok( fbq('10') == "Bar*", "Bar*" );
+});
+QUnit.test( "fbq('13')", function( assert ) {
+  assert.ok( fbq('13') == "Foo", "Foo" );
+});
+QUnit.test( "fbq('15')", function( assert ) {
+  assert.ok( fbq('15') == "FooBarBar", "FooBarBar (divisible by 3, divisible by 5, contains 5)" );
+});
+QUnit.test( "fbq('21')", function( assert ) {
+  assert.ok( fbq('21') == "FooQix", "FooQix" );
 });
 QUnit.test( "fbq('33')", function( assert ) {
   assert.ok( fbq('33') == "FooFooFoo", "FooFooFoo (divisible by 3, contains two 3)" );
 });
-
-QUnit.test( "fbq('100')", function( assert ) {
-  assert.ok( fbq('100') == "Bar**", "1** each 0 must be replace par char *" );
+QUnit.test( "fbq('51')", function( assert ) {
+  assert.ok( fbq('51') == "FooBar", "FooBar" );
 });
+QUnit.test( "fbq('53')", function( assert ) {
+  assert.ok( fbq('53') == "BarFoo", "BarFoo" );
+});
+
 QUnit.test( "fbq('101')", function( assert ) {
-  assert.ok( fbq('101') == "1*1", "1*1 each 0 must be replace par char *" );
+  assert.ok( fbq('101') == "1*1", "1*1" );
 });
-
+QUnit.test( "fbq('303')", function( assert ) {
+  assert.ok( fbq('303') == "FooFoo*Foo", "FooFoo*Foo" );
+});
+QUnit.test( "fbq('105')", function( assert ) {
+  assert.ok( fbq('105') == "FooBarQix*Bar", "FooBarQix*Bar" );
+});
+QUnit.test( "fbq('10101')", function( assert ) {
+  assert.ok( fbq('10101') == "FooQix**", "FooQix**" );
+});
 
 
 

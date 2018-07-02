5b31b2bdb03762896b47dc5e5c1c7a6973c86106
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-07-02 11:58:21 +0100

    testing divisors of 3 , test fail for numbers that contains 3 and not divisible by 3, also fails for number divisible by 3 and caintaining more occurence of 3

--- js/fbq.test.js
+++ js/fbq.test.js
@@ -17,9 +17,14 @@ QUnit.test( "fbq('3')", function( assert ) {
   assert.ok( fbq('3') == "FooFoo", "FooFoo (divisible by 3, contains 3)" );
 });
 
+QUnit.test( "fbq('32')", function( assert ) {
+  assert.ok( fbq('32') == "Foo", "Foo (contains 3)" );
+});
 QUnit.test( "fbq('33')", function( assert ) {
   assert.ok( fbq('33') == "FooFooFoo", "FooFooFoo (divisible by 3, contains two 3)" );
 });
+
+
 
 
 

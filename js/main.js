898dfc68d456d6a53ad9b1205fd828b2da594c2c
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-07-02 11:02:02 +0100

    Adding main.js file that contains the printing result function called main

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var main = (function () { //(Immediately Invoked Function Expression)
+    "use strict";
+   var div_results = document.querySelector("#fbq_div");
+    function run(cbk, x){
+		  const li = document.createElement("li");
+		const text = document.createTextNode(cbk(x));
+		li.appendChild(text);
+		div_results.appendChild(li);
+	}
+   
+   
+    function print(callback, limit) {
+        var values = Array.from({length: limit})
+		             .map((_, i) => i + 1)
+		             .map(x => run(callback,x));
+        }
+
+    return {
+        print
+    };
+}());

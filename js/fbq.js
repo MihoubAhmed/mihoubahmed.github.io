c4bbae47fef4e854a51e4da4e6e80fa069afd7d0
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-07-02 13:34:34 +0100

    cheking for more char occurences

--- js/fbq.js
+++ js/fbq.js
@@ -9,14 +9,17 @@ window.fbq = function fbq(value) {
 	
 	var result='';
 	
+	
 	if (value % 3 === 0) {
         result = "Foo";
     }
-    if (value === "3") {
-       result += "Foo";
-    }
 	
-	
+	// checking for more char occurences
+	 value.split("").forEach(function (num) { // forEach is the fastest array loop for chrome 60 
+        if (num === "3") {
+            result += "Foo";
+        }
+    });
 	
 	  return result.length === 0 ? value.toString() : result;
 	
